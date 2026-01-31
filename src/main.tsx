d843d75ddd7f75b8cb70ce26b6b95d336dcc4e88
praneeth00007 <2300031137@kluniversity.in>
2026-01-31 15:01:49 +0530

    Added For speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Providers } from './Providers.tsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
